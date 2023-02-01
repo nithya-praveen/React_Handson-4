e61a2aabd1cc96b490915801fb1cbff51f98d3e5
Nithya <111569028+nithya-praveen@users.noreply.github.com>
2023-02-01 22:39:32 +0530

    Add files via upload

---
+++ src/RouterComponents/Contact.js
@@ -0,0 +1,8 @@
+const Contact=()=>{
+    return(
+        <div>
+            <h2 style={{marginLeft:100,marginTop:30}}>Contact Us Page</h2>
+        </div>
+    )
+}
+export default Contact;
---
+++ src/RouterComponents/Home.js
@@ -0,0 +1,8 @@
+const Home=()=>{
+    return(
+        <div>
+            <h2 style={{marginLeft:100,marginTop:30}}>Home Page</h2>
+        </div>
+    )
+}
+export default Home;
